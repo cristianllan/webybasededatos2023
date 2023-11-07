119fbee056be1992a5c6eb8539ecb27565999d57
cristianllan <124410645+cristianllan@users.noreply.github.com>
2023-11-06 19:26:57 -0700

    Add files via upload

--- Parcial3/menu.php
+++ Parcial3/menu.php
@@ -17,7 +17,18 @@
             <li><a class="dropdown-item" href="inicio.php">Inicio</a></li>
             <li><a class="dropdown-item" href="consultarAlumnos.php">Consultar alumnos</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="RegistrarAlumno.php">Registar alumno</a></li>
+            <li><a class="dropdown-item" href="RegistrarAlumno.php">Registrar alumno</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Materias
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="inicio.php">Inicio</a></li>
+            <li><a class="dropdown-item" href="consultarMaterias.php">Consultar materias</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="RegistrarMaterias.php">Registrar materias</a></li>
           </ul>
         </li>
       </ul>
